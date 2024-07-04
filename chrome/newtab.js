25f06a731bdf16ecb64c863ffd6285cbea7f75e2
rooki <rooki@rooki.xyz>
2024-07-04 15:25:13 +0200

    Implemented part of it

--- chrome/newtab.js
+++ chrome/newtab.js
@@ -2,13 +2,78 @@ async function redirectNewTab() {
   const homepageUrl = "https://startyparty.dev";
 
   await chrome.windows.getCurrent(async (window) => {
-    await chrome.tabs.query({ active: true, windowId: window.id }, async (tabs) => {
-      for (const tab of tabs) {
-        chrome.tabs.update(tab.id, { url: homepageUrl });
+    await chrome.tabs.query(
+      { active: true, windowId: window.id },
+      async (tabs) => {
+        for (const tab of tabs) {
+          chrome.tabs.update(tab.id, { url: homepageUrl });
+        }
       }
-    });
+    );
   });
 }
 
 redirectNewTab();
 
+function resetIconAndBadge() {
+  chrome.action.setIcon({
+    path: "icon--alt.png",
+  });
+  chrome.action.setBadgeText({ text: "" });
+}
+
+async function fetchTokenOrRedirect(store) {
+  const token = await chrome.cookies.get({
+    url: "https://startyparty.dev/",
+    name: "__Secure-next-auth.session-token",
+  });
+
+  if (!token) {
+    await chrome.tabs.create({
+      url: homepageUrl + "login",
+      cookieStoreId: store,
+    });
+    return;
+  }
+  return token.value;
+}
+
+async function sendWebsiteToApi(tab) {
+  const url = tab.url;
+  const title = tab.title;
+  const favicon = tab.favIconUrl;
+  const data = { url, title, favicon };
+
+  const token = await fetchTokenOrRedirect(tab.cookieStoreId);
+
+  if (!token) {
+    // Doesnt work
+    chrome.action.setIcon({
+      path: "icon--alt_red.png",
+    });
+    setTimeout(resetIconAndBadge, 5000);
+    return;
+  }
+
+  // fetch("https://startyparty.dev/api", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Authorization": "Bearer " + token
+  //   },
+  //   body: JSON.stringify(data),
+  // });
+  chrome.action.setIcon({
+    path: "icon--alt_green.png",
+  });
+  setTimeout(resetIconAndBadge, 5000);
+}
+
+function handleClick() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const currentTab = tabs[0];
+    sendWebsiteToApi(currentTab);
+  });
+}
+
+chrome.action.onClicked.addListener(handleClick);
--- firefox/background.js
+++ firefox/background.js
@@ -19,3 +19,66 @@ browser.windows.onCreated.addListener(async (window) => {
     });
   }
 });
+
+function resetIconAndBadge() {
+  browser.browserAction.setIcon({
+    "path": "icon.svg"
+  });
+  browser.browserAction.setBadgeText({ text: "" });
+}
+
+async function fetchTokenOrRedirect(store) {
+  const token = await browser.cookies.get({
+    url: "https://startyparty.dev/",
+    name: "__Secure-next-auth.session-token",
+  });
+
+  if (!token) {
+    await browser.tabs.create({
+      url: homepageUrl + "login",
+      cookieStoreId: store,
+    });
+    return;
+  }
+  return token.value;
+}
+
+async function sendWebsiteToApi(tab) {
+  const url = tab.url;
+  const title = tab.title;
+  const favicon = tab.favIconUrl;
+  const data = { url, title, favicon };
+
+  const token = await fetchTokenOrRedirect(tab.cookieStoreId);
+
+  if (!token) {
+    // Todo fix this
+    browser.browserAction.setIcon({
+      path: "icon_red.svg"
+    })
+    setTimeout(resetIconAndBadge, 5000);
+    return;
+  }
+
+  // fetch("https://startyparty.dev/api", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Authorization": "Bearer " + token
+  //   },
+  //   body: JSON.stringify(data),
+  // });
+  browser.browserAction.setIcon({
+    "path": "icon_green.svg"
+  });
+  setTimeout(resetIconAndBadge, 5000);
+}
+
+function handleClick() {
+  browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const currentTab = tabs[0];
+    sendWebsiteToApi(currentTab);
+  });
+}
+
+browser.browserAction.onClicked.addListener(handleClick);
